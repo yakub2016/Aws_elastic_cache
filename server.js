d1b1903151428d4c3cb616faf25500af60324852
yakub2016 <spack.digi@gmail.com>
2026-07-07 17:27:40 +0530

    update

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Student Management API is running successfully!",
+  });
+});
+
+// Health Check Route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date(),
+  });
+});
 
 app.use("/students", require("./routes/student.route"));
 const PORT = process.env.PORT || 3000;
